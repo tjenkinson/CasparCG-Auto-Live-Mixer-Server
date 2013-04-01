dcd473617b4c5b486cfe5052bd4882d906d9209f
Tom Jenkinson <tjcode@outlook.com>
2013-04-01 01:16:30 +0100

    Fixed CaspSocket.java.

--- tjenkinson.caspar_audio_looper/src/tjenkinson/caspar_serverconnection/CaspSocket.java
+++ tjenkinson.caspar_audio_looper/src/tjenkinson/caspar_serverconnection/CaspSocket.java
@@ -3,6 +3,7 @@ package tjenkinson.caspar_serverconnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Date;
@@ -15,15 +16,18 @@ import tjenkinson.caspar_serverconnection.commands.CaspReturn;
 public class CaspSocket {
 	
 	private Socket socket = null;
+	private PrintWriter out = null;
 	private BufferedReader in = null;
 	
 	public CaspSocket(String caspAddress, int caspPort) throws IOException {
 		socket = new Socket(InetAddress.getByName(caspAddress), caspPort);
-		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));	
 	}
 	
 	
 	public synchronized CaspReturn runCmd(CaspCmd cmd) throws IOException {
+		out.println(cmd.getCmdString());
 		long requestTime = new Date().getTime();
 		
 		String line = in.readLine();
