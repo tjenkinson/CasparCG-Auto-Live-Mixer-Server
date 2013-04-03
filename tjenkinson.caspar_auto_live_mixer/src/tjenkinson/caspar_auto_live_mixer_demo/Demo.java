1190d53de72289ecf2083e1bc6cb7d7e6767da65
Tom Jenkinson <thomas_jenkinson@hotmail.co.uk>
2013-04-03 02:18:17 +0200

    Fixed indentation.

--- tjenkinson.caspar_auto_live_mixer/src/tjenkinson/caspar_auto_live_mixer_demo/Demo.java
+++ tjenkinson.caspar_auto_live_mixer/src/tjenkinson/caspar_auto_live_mixer_demo/Demo.java
@@ -23,70 +23,69 @@ public class Demo {
 
 	
 	public static void main(String[] args) throws InterruptedException, IOException {
-		// TODO Auto-generated method stub
 		
 		Socket socket = null;
-        PrintWriter out = null;
-        BufferedReader in = null;
-        int audioLooperPort = 5150;
-        String audioLooperAddress = "127.0.0.1";
-        // Setup the socket to connect to the audio looper server
-        try {
-            socket = new Socket(audioLooperAddress, audioLooperPort);
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (UnknownHostException e) {
-            System.exit(1);
-        } catch (IOException e) {
-            System.exit(1);
-        }
-
-        out.println("ADD 0 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 1 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 2 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 3 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 4 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 5 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 6 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 7 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 8 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 9 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 10 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 11 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 12 1 0");
-        System.out.println("return code: "+in.readLine());
-        out.println("ADD 13 0 1");
-        System.out.println("return code: "+in.readLine());
-        pressEnter();
-        
-        out.println("ADD 14 1 0");
-        System.out.println("return code: "+in.readLine());
-        
-        socket.close();
+	        PrintWriter out = null;
+	        BufferedReader in = null;
+	        int audioLooperPort = 5150;
+	        String audioLooperAddress = "127.0.0.1";
+	        // Setup the socket to connect to the audio looper server
+	        try {
+	            socket = new Socket(audioLooperAddress, audioLooperPort);
+	            out = new PrintWriter(socket.getOutputStream(), true);
+	            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        } catch (UnknownHostException e) {
+	            System.exit(1);
+	        } catch (IOException e) {
+	            System.exit(1);
+	        }
+	
+	        out.println("ADD 0 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 1 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 2 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 3 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 4 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 5 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 6 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 7 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 8 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 9 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 10 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 11 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 12 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        out.println("ADD 13 0 1");
+	        System.out.println("return code: "+in.readLine());
+	        pressEnter();
+	        
+	        out.println("ADD 14 1 0");
+	        System.out.println("return code: "+in.readLine());
+	        
+	        socket.close();
 	}
 	
 	private static void pressEnter() throws IOException, InterruptedException {
